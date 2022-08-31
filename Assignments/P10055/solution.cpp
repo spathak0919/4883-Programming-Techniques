c7747ee703e27b523f8cd599d0bb62ca8ce40d9c
Sandesh Pathak <70244866+spathak0919@users.noreply.github.com>
2022-08-30 19:25:22 -0500

    Update solution.cpp

--- Assignments/P10055/solution.cpp
+++ Assignments/P10055/solution.cpp
@@ -1,1 +1,23 @@
+/**
+* Name   : Sandesh Pathak
+* Course : 4883- Programming Techniques
+* Date   : 08/30/2022
+*/
+#include <iostream>
+using namespace std;
 
+int main() {
+    int B = 0, A = 0;
+    
+    while (cin >> A >> B) {
+        
+       int difference = (B-A);
+
+      if (difference<0)
+      difference*=-1;
+
+      cout <<difference<<endl;
+
+    }
+    return 0;
+}
