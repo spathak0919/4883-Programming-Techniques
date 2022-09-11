d3210a493086912f1bc8b17f476148ae99f67468
Sandesh Pathak <70244866+spathak0919@users.noreply.github.com>
2022-09-11 09:35:33 -0500

    Update main.cpp

--- Assignments/Easy Breezy/10783/main.cpp
+++ Assignments/Easy Breezy/10783/main.cpp
@@ -1,1 +1,28 @@
+// Name   - Sandesh Pathak
+// P10783 - Odd Sum
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int test;
+    cin >> test;
+    for (int r = 0; r < test; r++)
+      
+    {
+        int range1;
+        int range2;
+        int sum =0;
+        cin >> range1;
+        cin >> range2;
+
+        for(int x=range1; x<=range2; x++){
+            if(x%2!=0){
+                sum+=x;
+            }
+        }
+        cout << "Case " << (r+1) << ": " << sum <<endl;
+    }
+    return 0;
+}
